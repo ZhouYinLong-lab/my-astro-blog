43149b962fdfa93f1ff0d6493a565bbf1c03cfc2
LiuHanZhi <liuhanzhi514@gmail.com>
2026-02-24 23:08:29 +0800

    pnpm biome check --write ./src执行

--- src/content/config.ts
+++ src/content/config.ts
@@ -2,27 +2,29 @@ import { defineCollection, z } from "astro:content";
 
 const blog = defineCollection({
   type: "content",
-  schema: z.object({
-    title: z.string(),
-    description: z.string(),
-    pubDate: z.coerce.date(),
-    updated: z.coerce.date().optional(),
-    image: z.string().optional(),
-    badge: z.string().optional(),
-    draft: z.boolean().default(false),
-    categories: z
-      .array(z.string())
-      .refine((items: string[]) => new Set(items).size === items.length, {
-        message: "categories must be unique",
-      })
-      .optional(),
-    tags: z
-      .array(z.string())
-      .refine((items: string[]) => new Set(items).size === items.length, {
-        message: "tags must be unique",
-      })
-      .optional(),
-  }).catchall(z.any()), // 👉 终极魔法：接收并放行任何未来自定义的字段！
+  schema: z
+    .object({
+      title: z.string(),
+      description: z.string(),
+      pubDate: z.coerce.date(),
+      updated: z.coerce.date().optional(),
+      image: z.string().optional(),
+      badge: z.string().optional(),
+      draft: z.boolean().default(false),
+      categories: z
+        .array(z.string())
+        .refine((items: string[]) => new Set(items).size === items.length, {
+          message: "categories must be unique",
+        })
+        .optional(),
+      tags: z
+        .array(z.string())
+        .refine((items: string[]) => new Set(items).size === items.length, {
+          message: "tags must be unique",
+        })
+        .optional(),
+    })
+    .catchall(z.any()), // 👉 终极魔法：接收并放行任何未来自定义的字段！
 });
 
 export const collections = { blog };
